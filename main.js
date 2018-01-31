99de463cd54e6fa54bc73a822fd6c76c927d35d9
Jarek Pieprzak <jarekpiep@gmail.com>
2018-01-31 14:42:04 +0100

    create initial base of questions

---
+++ main.js
@@ -0,0 +1,21 @@
+var questions = [{
+    question: "What is the population of Poland?",
+    choices: ["100 million", "40 million", "1 million", "35 million"],
+    correctAnswer: 1
+}, {
+    question: "What is the population of England?",
+    choices: ["100 million", "40 million", "1 million", "35 million"],
+    correctAnswer: 2
+}, {
+    question: "What is the population of Germany?",
+    choices: ["100 million", "40 million", "1 million", "35 million"],
+    correctAnswer: 1
+}, {
+    question: "What is the population of Kambodia?",
+    choices: ["100 million", "40 million", "1 million", "35 million"],
+    correctAnswer: 0
+}, {
+    question: "What is the population of China?",
+    choices: ["100 million", "40 million", "1 million", "35 million"],
+    correctAnswer: 0
+}];
