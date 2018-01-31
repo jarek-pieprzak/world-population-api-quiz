d566434515368ef18baa5eecaf6a7bb6311a9a9f
Jarek Pieprzak <jarekpiep@gmail.com>
2018-01-31 17:05:44 +0100

    display question

--- main.js
+++ main.js
@@ -1,21 +1,43 @@
-var questions = [{
+const questions = [
+    {
     question: "What is the population of Poland?",
     choices: ["100 million", "40 million", "1 million", "35 million"],
     correctAnswer: 1
-}, {
+    },
+    {
     question: "What is the population of England?",
     choices: ["100 million", "40 million", "1 million", "35 million"],
     correctAnswer: 2
-}, {
+    },
+    {
     question: "What is the population of Germany?",
     choices: ["100 million", "40 million", "1 million", "35 million"],
     correctAnswer: 1
-}, {
+    },
+    {
     question: "What is the population of Kambodia?",
     choices: ["100 million", "40 million", "1 million", "35 million"],
     correctAnswer: 0
-}, {
+    },
+    {
     question: "What is the population of China?",
     choices: ["100 million", "40 million", "1 million", "35 million"],
     correctAnswer: 0
 }];
+
+const activeQuest = 0;
+
+$(document).ready(function () {
+
+    displayQuest();
+
+});
+
+function displayQuest() {
+
+    const question = questions[activeQuest].question;
+    const questClass = $(document).find(".quiz > .quest");
+
+    $(questClass).text(question);
+
+}
