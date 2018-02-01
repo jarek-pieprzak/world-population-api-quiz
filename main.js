0da5bb6d9d2c89e522dcdcfa46d18adc1bdceaf1
Jarek Pieprzak <jarekpiep@gmail.com>
2018-02-01 01:25:01 +0100

    displaying questions and answers, summing up correct and presenting results

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const quests = [
+var quests = [
     {
     quest: "What is the population of Poland?",
     answers: ["100 million", "40 million", "1 million", "35 million"],
@@ -6,50 +6,135 @@ const quests = [
     },
     {
     quest: "What is the population of England?",
-    answers: ["100 million", "40 million", "1 million", "35 million"],
+    answers: ["1 million", "2 million", "3 million", "4 million"],
     correct: 2
     },
     {
     quest: "What is the population of Germany?",
-    answers: ["100 million", "40 million", "1 million", "35 million"],
+    answers: ["5 million", "6 million", "7 million", "8 million"],
     correct: 1
     },
     {
     quest: "What is the population of Kambodia?",
-    answers: ["100 million", "40 million", "1 million", "35 million"],
+    answers: ["9 million", "56 million", "56 million", "10 million"],
     correct: 0
     },
     {
     quest: "What is the population of China?",
-    answers: ["100 million", "40 million", "1 million", "35 million"],
+    answers: ["100 million", "40 million", "1 million", "123123 million"],
     correct: 0
 }];
 
-const activeQuest = 0;
+var activeQuest = 0;
+var correctAnswers = 0;
+var end = false;
 
 $(document).ready(function () {
 
     displayQuest();
 
+    $(this)
+        .find(".alert")
+        .hide();
+
+    $(this)
+        .find(".next")
+        .on("click", function () {
+
+
+        if (!end) {
+
+        value = $("input[type='radio']:checked").val();
+
+            if (value == undefined) {
+                $(document)
+                    .find(".alert")
+                    .text("Please select an answer");
+
+                $(document)
+                    .find(".alert")
+                    .show();
+
+            } else {
+                $(document)
+                    .find(".alert")
+                    .hide();
+
+                if (value == quests[activeQuest].correct) {
+                    correctAnswers++;
+                }
+
+                activeQuest++;
+                if (activeQuest < quests.length) {
+                    displayQuest();
+                } else {
+                    displayScore();
+
+                    $(document)
+                        .find(".next")
+                        .text("Play Again?");
+
+                    end = true;
+                }
+            }
+        } else {
+            end = false;
+
+            $(document)
+                .find(".next")
+                .text("Next Question");
+
+
+            restart();
+            displayQuest();
+            hideScore();
+        }
+    });
+
 });
 
 function displayQuest() {
 
-    const quest = quests[activeQuest].quest;
-    const questClass = $(document).find(".quizBox > .quest");
-    const answerList = $(document).find(".quizBox > .answers");
-    const numChoices = quests[activeQuest].answers.length;
+    var quest = quests[activeQuest].quest;
+    var questClass = $(document).find(".quizBox > .quests");
+    var answers = $(document).find(".quizBox > .answers");
+    var numChoices = quests[activeQuest].answers.length;
 
     $(questClass).text(quest);
+
+    $(answers).find("li").remove();
 
     var choice;
     for (i = 0; i < numChoices; i++) {
         choice = quests[activeQuest].answers[i];
         $('<li><input type="radio" value='
-            + i +
-            ' name="dynradio" />'
-            + choice +
-            '</li>'
-        ).appendTo(answerList);
+            + i
+            + ' name="dynradio" />'
+            + choice
+            + '</li>')
+            .appendTo(answers);
     }
+}
+
+function restart() {
+    activeQuest = 0;
+    correctAnswers = 0;
+    hideScore();
+}
+
+function displayScore() {
+    $(document).
+        find(".quizBox > .result")
+        .text("Result: "
+            + correctAnswers
+            + "/"
+            + quests.length);
+
+    $(document).find(".quizBox > .result").show();
+}
+
+function hideScore() {
+    $(document)
+        .find(".result")
+        .hide();
 }
